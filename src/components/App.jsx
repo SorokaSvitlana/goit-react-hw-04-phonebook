2cd5eec76fd16b1dea498f7ec8038314168627fa
SorokaSvitlana <113341436+SorokaSvitlana@users.noreply.github.com>
2023-05-20 19:15:46 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,22 +5,38 @@ import { ContactList } from "./Contacts/Contacts";
 import { Search } from "./Search/Search";
 import { Container, Title, ListTitle } from './Container/Container.Styled'
 
-export function App () {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+export function App() {
+  const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
-  
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      setContacts(JSON.parse(savedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
+  };
+
   const handleSubmit = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     const isNameExists = contacts.some(
       (contact) => contact.name.toLowerCase() === name.toLowerCase()
-    )
+    );
+
     if (isNameExists) {
       alert(`${name} is already in contacts.`);
       return;
@@ -36,15 +52,6 @@ export function App () {
       setContacts((prevContacts) => [...prevContacts, contact]);
       setName('');
       setNumber('');
-    }
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    if (name === 'name') {
-      setName(value);
-    } else if (name === 'number') {
-      setNumber(value);
     }
   };
 
@@ -67,16 +74,19 @@ export function App () {
   };
 
   const filteredContacts = getFilteredContacts();
+
   return (
-   
-     <Container>
+    <Container>
       <Title>Phonebook</Title>
-<Form name={name}
-number={number}
-handleInputChange={handleInputChange}
- handleSubmit={handleSubmit}></Form>
-<ListTitle>Contacts</ListTitle>
-<Search filter= {filter} handleFilterChange = {handleFilterChange}></Search>
-<ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact}> </ContactList> 
-    </Container>  )
-  } 
+      <Form
+        name={name}
+        number={number}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+      />
+      <ListTitle>Contacts</ListTitle>
+      <Search filter={filter} handleFilterChange={handleFilterChange} />
+      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
+    </Container>
+  );
+}
